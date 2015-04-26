e8449af9b42a0602e3cb35a3f6f5c274532b27d3
Kryztof Urban <kryztof@gumgum.com>
2015-04-26 16:19:43 +0200

    Adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## Basic functions for cache matrix creation
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -10,24 +9,29 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
+  ## set inverse
   setinverse <- function(mean) m <<- solve
+  ## get inverse
   getinverse <- function() m
+  ## list available functions
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## return cached data
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
   if(!is.null(m)) {
+    ## cache already set - return
     message("getting cached data")
     return(m)
   }
+  ## cache not set yet - populate with inverse of matrix
   data <- x$get()
   m <- solve(data, ...)
   x$setinverse(m)
+  ## return cache contents
   m
 }
